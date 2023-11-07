312745619817c3c6efd688ff44a8777a4cac768e
Vladimir <vovachempogir@mail.ru>
2023-11-07 22:15:35 +0700

    init

---
+++ src/main/java/com/gridnine/testing/GridnineApplication.java
@@ -0,0 +1,13 @@
+package com.gridnine.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridnineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GridnineApplication.class, args);
+    }
+
+}
